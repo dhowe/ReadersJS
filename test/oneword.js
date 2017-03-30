dfd2a73e85ea73d7d93442d3d177cd20133839e3
John Cayley <John_Cayley@brown.edu>
2017-03-30 09:55:00 -0400

    sorting out

--- test/oneword.js
+++ test/oneword.js
@@ -15,13 +15,13 @@ function setup() {
   rt =  RiText('Hello', 30, 50);
   rt2 =  RiText('there', 150, 50);
 
-  rt.colorTo({r:255,g:0,b:0,a:255},4);
+  rt.colorTo({r:255,g:0,b:0,a:255},2); // should fade in over 2 seconds - doesn't
   // two argument version fails -> immediate red instead of in 4 seconds
-  rt.colorTo({r:0,g:255,b:0,a:255},2,0.01); // this works - green
-  rt.colorTo({r:0,g:0,b:255,a:255},2,4); // this works - blue
-  rt.colorTo({r:0,g:0,b:255,a:127},2,8); // this works - dark blue
-  rt.colorTo({r:255,g:0,b:0,a:127},2,12); // this works - dark red
-  rt.colorTo({r:255,g:0,b:0,a:255},2,16); // this works - red
+  rt.colorTo({r:0,g:255,b:0,a:255},2, 4); // this works - green
+  rt.colorTo({r:0,g:0,b:255,a:255},2, 8); // this works - blue
+  rt.colorTo({r:0,g:0,b:255,a:127},2, 12); // this works - dark blue
+  rt.colorTo({r:255,g:0,b:0,a:127},2, 16); // this works - dark red
+  rt.colorTo({r:255,g:0,b:0,a:255},2, 20); // this works - red
 
   rt2.colorTo([0,0,255,255],2, .00001);
   rt2.colorTo([255,0,0,255],.01, 1); // fade slowly to blue
