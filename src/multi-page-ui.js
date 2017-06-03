c01c44c13c921cf097c1d6e2fbb9adc18993253b
dhowe <daniel@rednoise.org>
2017-06-04 01:58:37 +0800

    refactors to support sublassing of Reader.outputAsHTML function

--- src/multi-page-ui.js
+++ src/multi-page-ui.js
@@ -6,7 +6,7 @@ var SPEED = {
   Slow: 1.2,
   Slower: 1.6,
   Slowest: 2.0,
-  Fast: 0.2,
+  Fast: 0.2
 };
 
 var STYLE = {
@@ -22,76 +22,87 @@ var COLOR = {
 
 var bgColor = 0,
   uiLogging = true,
+  activeReadersBeforeSolo,
   maxFocusLog = Math.floor(window.innerHeight / 30);
 
-var activeReadersBeforeSolo;
-
 function createInterface() {
-
-  Reader.instances.forEach(function (reader) {
-
+  Reader.instances.forEach(function(reader) {
     var rb = createCheckbox(reader.name(), !reader.hidden);
     rb.class("reader");
     rb.id(reader.type);
-    rb.parent('interface');
+    rb.parent("interface");
 
-    var focusButton = createCheckbox('F',false);
-    focusButton.class('smallButton focus');
-    focusButton.attribute('title','focus');
+    var focusButton = createCheckbox("F", false);
+    focusButton.class("smallButton focus");
+    focusButton.attribute("title", "focus");
     focusButton.changed(focusButtonPushed);
     focusButton.value(reader.type);
     focusButton.parent(rb);
 
-    var soloButton = createCheckbox('S',false);
-    soloButton.class('smallButton solo');
-    soloButton.attribute('title','solo');
+    var soloButton = createCheckbox("S", false);
+    soloButton.class("smallButton solo");
+    soloButton.attribute("title", "solo");
     soloButton.changed(soloButtonPushed);
     soloButton.value(reader.type);
     soloButton.parent(rb);
 
-    var selectWrapper =  createDiv('');
-    selectWrapper.class('select');
+    var selectWrapper = createDiv("");
+    selectWrapper.class("select");
     selectWrapper.parent(rb);
 
-    var speedSelect = initSelect('speedSelect', 'full', Object.keys(SPEED), speedChanged, selectWrapper);
+    var speedSelect = initSelect(
+      "speedSelect",
+      "full",
+      Object.keys(SPEED),
+      speedChanged,
+      selectWrapper
+    );
     speedSelect.value(speedToName(reader.speed));
     speedSelect.source = reader;
-
 
     // rb.child(document.getElementById('hoverTextWrapper').cloneNode(true)); // onhover message
   });
 
-  var textSelect, styleSelect, themeSelect, uiElements = [
-    textSelect = initSelect('textSelect', 'full', textNames(), textChanged),
-    styleSelect = initSelect('styleSelect', 'half', Object.keys(STYLE), themeChanged),
-    themeSelect = initSelect('themeSelect', 'half', ['Dark', 'Light'], themeChanged),
-  ];
+  var textSelect,
+    styleSelect,
+    themeSelect,
+    uiElements = [
+      (textSelect = initSelect("textSelect", "full", textNames(), textChanged)),
+      (styleSelect = initSelect(
+        "styleSelect",
+        "half",
+        Object.keys(STYLE),
+        themeChanged
+      )),
+      (themeSelect = initSelect(
+        "themeSelect",
+        "half",
+        ["Dark", "Light"],
+        themeChanged
+      ))
+    ];
 
   // set initial class
   styleSelect.value("Faint");
   var focused = pManager.focus();
 
   if (focused) {
-
     if (focused.hidden) {
-
       focused = randomReader();
-      warn("Focus repair" + (focused ? ': ' + focused.type :
-        " failed: " + (activeReaders().length + " readers"))); // FIX ME
+      warn("Focus repair" + (focused ? ": " + focused.type // FIX ME
+            : " failed: " + (activeReaders().length + " readers")));
     }
-
     focusChanged(focused);
   }
 
   // Append elements to interface
-  var descText = ['Text', 'Style', 'Theme'];
+  var descText = ["Text", "Style", "Theme"];
   for (var i = 0; i < uiElements.length; i++) {
-
-    var wrapper = createDiv('');
-    wrapper.addClass('item').parent('interface');
+    var wrapper = createDiv("");
+    wrapper.addClass("item").parent("interface");
     createP(descText[i]).parent(wrapper);
-    var selectWrapper =  createDiv('');
-    selectWrapper.class('select');
+    var selectWrapper = createDiv("");
+    selectWrapper.class("select");
     uiElements[i].parent(selectWrapper);
     wrapper.child(selectWrapper);
   }
@@ -103,16 +114,14 @@ function createInterface() {
   ////////////////////////////////////////////////////////////////////
 
   function textNames() {
-
     var names = [];
-    TEXTS.forEach(function (text) {
+    TEXTS.forEach(function(text) {
       names.push(text.title);
     });
     return names;
   }
 
   function initSelect(id, style, options, onChanged, parent) {
-
     var sel = createSelect();
     for (var i = 0; i < options.length; i++)
       sel.option(options[i]);
@@ -122,19 +131,15 @@ function createInterface() {
   }
 
   // function styleChanged() {
-  //
   //   var col = (bgColor == 0) ? COLOR.White : COLOR.Black,
   //     name = styleSelect.value(),
   //     style = STYLE[name];
-  //
   //   log("[UI] Style: " + name + "/" + style);
   //   pManager.gridFill(colorToObject(col, style));
   // }
 
   function resetFocus() {
-
-    var focused = pManager.focus(),
-      actives = activeReaders();
+    var focused = pManager.focus(), actives = activeReaders();
 
     // if only one reader is active and its not focused, give it focus
     if (actives.length == 1 && actives[0] !== focused) {
@@ -149,33 +154,31 @@ function createInterface() {
   }
 
   function assignFocus(focused) {
-
     if (!focused) {
-
       var actives = activeReaders(); // pick a random reader for focus
       focused = actives.length && actives[floor(random(actives.length))];
     }
 
     clearFocus();
 
-    if (focused) { // only if we have an active reader
-
+    if (focused) {
+      // only if we have an active reader
       document.getElementById(focused.type).className += " focused";
       pManager.focus(focused);
       pManager.makeFocusedReaderVisible();
     }
 
     // clear focusDisplay, change color
-    $('#focusDisplay').html('');
-
-    $('#focusDisplayTag').css("color", focused ? getCSSFromColor(focused.activeFill) : "#EEE");
+    $("#focusDisplay").html("");
+    $("#focusDisplayTag").css("color", focused ?
+      getCSSFromColor(focused.activeFill) : "#EEE"
+    );
   }
 
   function clearFocus() {
-
     var rdrs = document.getElementsByClassName("reader");
     for (var i = 0; i < rdrs.length; i++) {
-      rdrs[i].className = rdrs[i].className.replace(" focused", '');
+      rdrs[i].className = rdrs[i].className.replace(" focused", "");
     }
   }
 
@@ -186,95 +189,97 @@ function createInterface() {
     if (document.getElementById(reader.type).className === "reader solo")
       unsolo(reader);
 
-    log("[UI] " + reader.type + (reader.hidden ? ': Off' : ': On'));
+    log("[UI] " + reader.type + (reader.hidden ? ": Off" : ": On"));
   }
 
   function focusChanged(focused) {
-      log("[UI] Focus: " + focused.type);
-      focused && pManager.focus(focused);
-      assignFocus(focused);
+    log("[UI] Focus: " + focused.type);
+    focused && pManager.focus(focused);
+    assignFocus(focused);
 
-      // turn off other focusButtons
-      var eles = document.getElementsByClassName('smallButton focus');
+    // turn off other focusButtons
+    var eles = document.getElementsByClassName("smallButton focus");
 
-      for (var i = 0; i < eles.length; i++) {
-              eles[i].firstElementChild.checked = eles[i].parentNode.id === focused.type ? true : false;
-      }
+    for (var i = 0; i < eles.length; i++) {
+      eles[i].firstElementChild.checked = eles[i].parentNode.id === focused.type
+        ? true : false;
+    }
   }
 
   function focusButtonPushed() {
     if (this.elt.firstElementChild.checked) {
-        focusChanged(Reader.firstOfType(this.elt.parentElement.id));
+      focusChanged(Reader.firstOfType(this.elt.parentElement.id));
     } else {
-        // if it is the current focus, do nothing // Or random select another one ?
-        this.elt.firstElementChild.checked = true;
+      // if it is the current focus, do nothing (or random select another one?)
+      this.elt.firstElementChild.checked = true;
     }
-}
+  }
 
   function soloButtonPushed() {
-      var targetReader = Reader.firstOfType(this.elt.parentElement.id);
-      if (this.elt.firstElementChild.checked)
-        solo(targetReader);
-      else
-        unsolo(targetReader);
+    var targetReader = Reader.firstOfType(this.elt.parentElement.id);
+    if (this.elt.firstElementChild.checked) solo(targetReader);
+    else unsolo(targetReader);
   }
 
   function solo(targetReader) {
-      var readers = document.getElementsByClassName("reader");
+    var readers = document.getElementsByClassName("reader");
 
-      // disable all other readers, turn off active readers in the background
-      for (var i = 0; i < readers.length; i++) {
-          var reader = Reader.firstOfType(readers[i].id);
-          if (reader != targetReader) {
-              readers[i].className = "reader disabled";
-                readerOnOffEvent(reader, false);
-              //turn off other solo Buttons
-              readers[i].getElementsByClassName('smallButton solo')[0].firstElementChild.checked = false;
-          } else {
-            readerOnOffEvent(reader, true);
-            readers[i].className = "reader solo";
-          }
+    // disable all other readers, turn off active readers in the background
+    for (var i = 0; i < readers.length; i++) {
+      var reader = Reader.firstOfType(readers[i].id);
+      if (reader != targetReader) {
+        readers[i].className = "reader disabled";
+        readerOnOffEvent(reader, false);
+        //turn off other solo Buttons
+        readers[i].getElementsByClassName(
+          "smallButton solo"
+        )[0].firstElementChild.checked = false;
+      } else {
+        readerOnOffEvent(reader, true);
+        readers[i].className = "reader solo";
       }
+    }
 
-      // change focus to the current reader
-      focusChanged(targetReader);
-      document.getElementById(targetReader.type).getElementsByClassName('smallButton focus')[0].firstElementChild.checked = true;
-
+    // change focus to the current reader
+    focusChanged(targetReader);
+    document
+      .getElementById(targetReader.type)
+      .getElementsByClassName("smallButton focus"
+      )[0].firstElementChild.checked = true;
   }
 
   function unsolo(targetReader) {
     // turn on other readers
     var readers = document.getElementsByClassName("reader");
 
-      for (var i = 0; i < readers.length; i++) {
-          var reader = Reader.firstOfType(readers[i].id);
-          readers[i].className = "reader";
-          if (reader === targetReader) {
-            readers[i].getElementsByClassName('smallButton solo')[0].firstElementChild.checked = false;
-          }
-          //if the reader is checked in interface, turn it on
-          if (readers[i].firstElementChild.checked === true) {
-              readerOnOffEvent(reader, true);
-          }
-
+    for (var i = 0; i < readers.length; i++) {
+      var reader = Reader.firstOfType(readers[i].id);
+      readers[i].className = "reader";
+      if (reader === targetReader) {
+        readers[i].getElementsByClassName(
+          "smallButton solo"
+        )[0].firstElementChild.checked = false;
       }
+      //if the reader is checked in interface, turn it on
+      if (readers[i].firstElementChild.checked === true) {
+        readerOnOffEvent(reader, true);
+      }
+    }
   }
 
   function speedToName(spd) {
-
     var result;
-    Object.keys(SPEED).forEach(function (name) {
-      if (SPEED[name] === spd)
-        result = name;
+    Object.keys(SPEED).forEach(function(name) {
+      if (SPEED[name] === spd) result = name;
     });
-    if (!result) throw Error('unknown speed: ' + spd);
+    if (!result) throw Error("unknown speed: " + spd);
     return result;
   }
 
   function speedChanged() {
     var spd = this.value();
     this.source.speed = SPEED[spd];
-    log('[UI] ' + this.source.type + '.speed=' + this.source.speed);
+    log("[UI] " + this.source.type + ".speed=" + this.source.speed);
   }
 
   function log() {
@@ -283,42 +288,49 @@ function createInterface() {
 
   ////////////////////////////////////////////////////////////////////
 
-  window.onresize = function () {
-
+  window.onresize = function() {
     // recalculate maxLog when window height changes
     maxFocusLog = Math.floor(window.innerHeight / 30);
   };
 
-  document.getElementById('options').addEventListener('click', function () {
-
-    if (menu.offsetHeight === 0) {
-      menu.style.display = 'block';
-      instructions.style.visibility = 'hidden';
-      options.classList = '';
-
-    } else {
-      menu.style.display = 'none';
-      instructions.style.visibility = 'visible';
-      options.classList = "clear";
-    }
-
-  }, false);
+  document.getElementById("options").addEventListener(
+    "click",
+    function() {
+      if (menu.offsetHeight === 0) {
+        menu.style.display = "block";
+        instructions.style.visibility = "hidden";
+        options.classList = "";
+      } else {
+        menu.style.display = "none";
+        instructions.style.visibility = "visible";
+        options.classList = "clear";
+      }
+    },
+    false
+  );
 
   // hide interface
-  document.getElementsByTagName('body')[0].addEventListener('click', function (event) {
-    var ui = document.getElementById("interface");
-    if (ui.offsetHeight > 0 && ui.offsetWidth > 0 &&
-      (event.pageX > ui.offsetWidth || event.pageY > ui.offsetHeight))
-      ui.style.display = 'none';
-  }, false);
+  document.getElementsByTagName("body")[0].addEventListener(
+    "click",
+    function(event) {
+      var ui = document.getElementById("interface");
+      if (
+        ui.offsetHeight > 0 &&
+        ui.offsetWidth > 0 &&
+        (event.pageX > ui.offsetWidth || event.pageY > ui.offsetHeight)
+      )
+        ui.style.display = "none";
+    },
+    false
+  );
 
   ////////////////////////////////////////////////////////////////////
 
   function onReaderSingleClick(ele) {
     var reader = Reader.firstOfType(ele.parentNode.id),
-         state = !ele.parentNode.getElementsByTagName('input')[0].checked;
+      state = !ele.parentNode.getElementsByTagName("input")[0].checked;
     readerOnOffEvent(reader, state);
- }
+  }
 
   // Remove DoubleClick
   // function onReaderDoubleClick(ele) {
@@ -333,42 +345,40 @@ function createInterface() {
   //     focusChanged(Reader.firstOfType(ele.parentNode.id));
   // }
 
-  menu.addEventListener('click', function (event) {
-
+  menu.addEventListener("click", function(event) {
     var el = event.target;
     // differeniate single & double click for reader
-    if (!el.matches('.reader > label')) return;
+    if (!el.matches(".reader > label")) return;
 
     // if (el.getAttribute("data-dblclick") == null) {
-      // el.setAttribute("data-dblclick", 1);
-      // setTimeout(function () {
-      //   if (el.getAttribute("data-dblclick") == 1) {
-          onReaderSingleClick(el);
+    // el.setAttribute("data-dblclick", 1);
+    // setTimeout(function () {
+    //   if (el.getAttribute("data-dblclick") == 1) {
+    onReaderSingleClick(el);
     //     }
     //     el.removeAttribute("data-dblclick");
     //   }, 300);
-
     // } else {
-
     //   el.removeAttribute("data-dblclick");
     //   onReaderDoubleClick(el);
     // }
-  })
+  });
 
-  menu.addEventListener('click', function (event) {
-
+  menu.addEventListener("click", function(event) {
     var ele = event.target;
-    if (ele.matches('.hoverText a.help')) {
-
-      var display = ele.parentNode.parentNode.getElementsByClassName("helpInfo")[0].style.display;
+    if (ele.matches(".hoverText a.help")) {
+      var display = ele.parentNode.parentNode.getElementsByClassName(
+        "helpInfo"
+      )[0].style.display;
       display = display === "block" ? "none" : "block";
-      ele.parentNode.parentNode.getElementsByClassName("helpInfo")[0].style.display = display;
+      ele.parentNode.parentNode.getElementsByClassName(
+        "helpInfo"
+      )[0].style.display = display;
     }
   });
 
   // No more in use
   // menu.addEventListener('mouseover', function (event) {
-
   //   var ele = event.target;
   //   if (ele.matches('.reader label') && !timeoutId) {
   //     timeoutId = window.setTimeout(function () {
@@ -378,43 +388,28 @@ function createInterface() {
   //   }
   // })
 
-  menu.addEventListener('mouseout', function (event) {
-
+  menu.addEventListener("mouseout", function(event) {
     if (timeoutId) {
       window.clearTimeout(timeoutId);
       timeoutId = null;
     }
     var helpInfos = document.getElementsByClassName("helpInfo");
-    for (var i = 0; i < helpInfos.length; i++)
-      helpInfos[i].style.display = "none";
+    for (
+      var i = 0;
+      i < helpInfos.length;
+      i++
+    ) helpInfos[i].style.display = "none";
   });
 
 } // end createInterface
 
 function focusJump(focused) {
-
-  $('#focusDisplay').html('');
+  $("#focusDisplay").html("");
 }
 
 function getCSSFromColor(colorObj) {
   if (colorObj === undefined) return null;
   else return "rgb(" + colorObj.r + "," + colorObj.g + "," + colorObj.b + ")";
-}
-
-function logToDisplay(msg) {
-
-  createP(msg).parent('focusDisplay');
-
-  //remove first element from focusDisplay
-  var display = document.getElementById("focusDisplay");
-  var logEntries = display.childNodes.length;
-
-  if (logEntries > maxFocusLog) {
-    while (logEntries > maxFocusLog) {
-      display.removeChild(display.childNodes[0]);
-      logEntries--;
-    }
-  }
 }
 
 function pauseInterface(val) {
@@ -423,8 +418,8 @@ function pauseInterface(val) {
 }
 
 // handle cases where tab is not the active tab, sleep, screensaver
-document.addEventListener('visibilitychange', function() {
-  !document.hidden && console.log('[UI] Tab reactivated');
+document.addEventListener("visibilitychange", function() {
+  if (!document.hidden) console.log("[UI] Tab reactivated");
   pauseInterface(document.hidden);
 });
 
@@ -442,29 +437,25 @@ $(window).focus(function() {
 });
 */
 
-$(document).ready(function () {
-
-  $('#focusDisplayTag').click(function () {
-    var tag = $('#focusDisplay:visible').length === 0 ? " - " : " + ";
-    $('#focusDisplay').toggle("slide");
-    $('#status').html(tag);
+$(document).ready(function() {
+  $("#focusDisplayTag").click(function() {
+    var tag = $("#focusDisplay:visible").length === 0 ? " - " : " + ";
+    $("#focusDisplay").toggle("slide");
+    $("#status").html(tag);
   });
 
-  $("body").on("click", "ul.select li.init", function () {
-
+  $("body").on("click", "ul.select li.init", function() {
     // hide other select list if opened
-    $('ul').children('li:not(.init)').hide();
-    $('ul').children('li.init').show();
-    $(this).closest("ul").children('li').toggle();
+    $("ul").children("li:not(.init)").hide();
+    $("ul").children("li.init").show();
+    $(this).closest("ul").children("li").toggle();
   });
 
-  $("body").on("click", "ul.select li:not(.init)", function () {
-
-    var allOptions = $("ul").children('li:not(.init)');
-    allOptions.removeClass('selected');
-    $(this).addClass('selected');
-    $(this).closest("ul").children('.init').html($(this).html());
-    $(this).closest("ul").children('li').toggle();
+  $("body").on("click", "ul.select li:not(.init)", function() {
+    var allOptions = $("ul").children("li:not(.init)");
+    allOptions.removeClass("selected");
+    $(this).addClass("selected");
+    $(this).closest("ul").children(".init").html($(this).html());
+    $(this).closest("ul").children("li").toggle();
   });
-
 });
--- src/readers-app.js
+++ src/readers-app.js
@@ -893,6 +893,7 @@ Reader.VARY_SPEED_BY_LETTER_COUNT = true; // flag for this enhancement
 
 // punctuation not included. "’" (right curly quote is part of the character set
 Reader.CLOSED_CLASS_WORDS = ["the", "and", "a", "of", "in", "i", "you", "is", "to", "that", "it", "for", "on", "have", "with", "this", "be", "not", "are", "as", "was", "but", "or", "from", "my", "at", "if", "they", "your", "all", "he", "by", "one", "me", "what", "so", "can", "will", "do", "an", "about", "we", "just", "would", "there", "no", "like", "out", "his", "has", "up", "more", "who", "when", "don’t", "some", "had", "them", "any", "their", "it’s", "only", "which", "i’m", "been", "other", "were", "how", "then", "now", "her", "than", "she", "well", "also", "us", "very", "because", "am", "here", "could", "even", "him", "into", "our", "much", "too", "did", "should", "over", "want", "these", "may", "where", "most", "many", "those", "does", "why", "please", "off", "going", "its", "i’ve", "down", "that’s", "can’t", "you’re", "didn’t", "another", "around", "must", "few", "doesn’t", "every", "yes", "each", "maybe", "i’ll", "away", "doing", "oh", "else", "isn’t", "he’s", "there’s", "hi", "won’t", "ok", "they’re", "yeah", "mine", "we’re", "what’s", "shall", "she’s", "hello", "okay", "here’s", "less"];
+
 Reader.STOP_WORDS = ["a", "about", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also", "although", "always", "am", "among", "amongst", "amount", "an", "and", "another", "any", "anyhow", "anyone", "anything", "anyway", "anywhere", "are", "around", "as", "at", "back", "be", "became", "because", "become", "becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom", "but", "by", "call", "can", "cannot", "cant", "co", "computer", "con", "could", "couldn’t", "cry", "de", "describe", "detail", "do", "done", "does", "down", "due", "during", "each", "eg", "eight", "either", "eleven", "else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fifty", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasn’t", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "i", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own", "part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thick", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves"];
 
 Reader.DEBUG_CREATES = 0;
@@ -1102,9 +1103,8 @@ Reader.prototype = {
 
       this.pman.notifyServer && (this.pman.sendUpdate(this, msg));
 
-      if (!this.hidden && this.hasFocus() && typeof createP === 'function') {
-        //console.log(msg);
-        logToDisplay(msg.replace(/ /g, "&nbsp;").replace(/\n/g, "<br>"));
+      if (!this.hidden && this.hasFocus()) {
+        this.outputAsHTML(msg);
       }
 
       reader.stepTime = this.speed * 1000; // to milliseconds
@@ -1122,6 +1122,37 @@ Reader.prototype = {
     setTimeout(function () {
       reader.step();
     }, reader.stepTime);
+  },
+
+  outputAsHTML: function (msg) {
+
+    // replace spaces and line-breaks with html versions
+    msg =  msg.replace(/ /g, '&nbsp;').replace(/\n/g, '<br>');
+
+    if (typeof createP === "function") {
+
+      // create a <p> tag as last element in 'focusDisplay'
+      createP(msg).parent("focusDisplay");
+
+      var display = document.getElementById("focusDisplay");
+
+      if (!(display && display.childNodes && display.childNodes.length)) {
+        return;
+      }
+
+      // now scroll up if necessary
+      var logEntries = display.childNodes.length;
+      while (logEntries > maxFocusLog) {
+
+        display.removeChild(display.childNodes[0]);
+        logEntries--;
+      }
+    }
+  },
+
+  formatMessage: function (msg) {
+
+    return
   },
 
   hasFocus: function () {
