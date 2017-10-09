f866e06b1e086eef092777dfae3f637cf3d3c931
John Cayley <John_Cayley@brown.edu>
2017-10-10 06:41:38 +0800

    Chinese default opening

---
+++ data/theImage_ch-trigrams.js
@@ -0,0 +1,10 @@
+if (typeof Trigrams === 'undefined') var Trigrams = {};
+Trigrams.TheImage = {
+'舌 头 上':1,
+'头 上 带':1
+}
+
+if (typeof module != 'undefined' && module.exports) { // for node
+  module.exports = Trigrams;
+}
+
