56b380ba9d37262acfb5ace3c5018de861edc92f
John Cayley <John_Cayley@brown.edu>
2017-10-26 10:02:04 -0400

    first cn-bigrams

--- xingxiang_workingfolder/cn_bigrammer.py
+++ xingxiang_workingfolder/cn_bigrammer.py
@@ -1,3 +1,12 @@
-with open ("image_cn_spcs_notags.txt", "r") as myfile:
-    text=myfile.readlines()
-print(text)
+import nltk
+f = open('image_cn_spcs_notags.txt')
+raw = f.read()
+characters = nltk.word_tokenize(raw)
+bgs = nltk.bigrams(characters)
+fdist = nltk.FreqDist(bgs)
+outfile = open('cn-bigrams.js','w')
+print("var cnBigrams = {", file=outfile)
+for k,v in fdist.items():
+    print('"' + k[0] + ' ' + k[1] + '": ' + str(v) + ',', file=outfile)
+print("};", file=outfile)
+outfile.close()
